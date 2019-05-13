a390dc21dc9e317f84f92ce194dfb2efd4a0a20e
Mitchell Williams <mcwil15@student.monash.edu>
2019-05-13 21:53:33 +1000

    Small overhaul to readability
    Removed the write accelerometer loop, improved readability.

--- Arduino_Code/Esp32NMEA/Esp32NMEA.ino
+++ Arduino_Code/Esp32NMEA/Esp32NMEA.ino
@@ -46,8 +46,8 @@ int ledPin = 13;
 void setup()
 {
   delay(2000);
-  DEBUG_PORT.begin(115200);
 
+  DEBUG_PORT.begin(115200);
 
   i2cSetup();
   enableGyroAcc();
@@ -57,55 +57,12 @@ void setup()
   pinMode(ledPin, OUTPUT);    //define LED pin
   digitalWrite(ledPin, HIGH); // Turn on LED pin
 
-  while (!DEBUG_PORT)
-    ;
-
-  DEBUG_PORT.print( F("NMEA.INO: started\n") );
-  DEBUG_PORT.print( F("  fix object size = ") );
-  DEBUG_PORT.println( sizeof(gps.fix()) );
-  DEBUG_PORT.print( F("  gps object size = ") );
-  DEBUG_PORT.println( sizeof(gps) );
-  DEBUG_PORT.println( F("Looking for GPS device on " GPS_PORT_NAME) );
-
-
-#ifndef NMEAGPS_RECOGNIZE_ALL
-#error You must define NMEAGPS_RECOGNIZE_ALL in NMEAGPS_cfg.h!
-#endif
-
-#ifdef NMEAGPS_INTERRUPT_PROCESSING
-#error You must *NOT* define NMEAGPS_INTERRUPT_PROCESSING in NMEAGPS_cfg.h!
-#endif
-
-#if !defined( NMEAGPS_PARSE_GGA ) & !defined( NMEAGPS_PARSE_GLL ) & \
-      !defined( NMEAGPS_PARSE_GSA ) & !defined( NMEAGPS_PARSE_GSV ) & \
-      !defined( NMEAGPS_PARSE_RMC ) & !defined( NMEAGPS_PARSE_VTG ) & \
-      !defined( NMEAGPS_PARSE_ZDA ) & !defined( NMEAGPS_PARSE_GST )
-
-  DEBUG_PORT.println( F("\nWARNING: No NMEA sentences are enabled: no fix data will be displayed.") );
-
-#else
-  if (gps.merging == NMEAGPS::NO_MERGING) {
-    DEBUG_PORT.print  ( F("\nWARNING: displaying data from ") );
-    DEBUG_PORT.print  ( gps.string_for( LAST_SENTENCE_IN_INTERVAL ) );
-    DEBUG_PORT.print  ( F(" sentences ONLY, and only if ") );
-    DEBUG_PORT.print  ( gps.string_for( LAST_SENTENCE_IN_INTERVAL ) );
-    DEBUG_PORT.println( F(" is enabled.\n"
-                          "  Other sentences may be parsed, but their data will not be displayed.") );
-  }
-#endif
-
-  DEBUG_PORT.print  ( F("\nGPS quiet time is assumed to begin after a ") );
-  DEBUG_PORT.print  ( gps.string_for( LAST_SENTENCE_IN_INTERVAL ) );
-  DEBUG_PORT.println( F(" sentence is received.\n"
-                        "  You should confirm this with NMEAorder.ino\n") );
-
-  trace_header( DEBUG_PORT );
-  DEBUG_PORT.flush();
+  neosetup();
 
   RFDPort.begin( 115200, SERIAL_8N1, TXRFD, RXRFD );
   delay(100);
 
-  gpsSetup();
+  gpsSetup(); //configures an MTK3339 based GPS (Adafruit Ultimate) -- See other tab
 }
 
 void loop()
@@ -125,97 +82,98 @@ static void doSomeWork()
 {
   // Print all the things!
 
-  trace_all( DEBUG_PORT, gps, fix );
-  gpsprint();
+  // trace_all( DEBUG_PORT, gps, fix );
+  gpsprint(RFDPort);
+
 }
 
 
 //------------------------------------
 //  This is the main GPS parsing loop.
-
 static void GPSloop()
 {
   while (gps.available( gpsPort )) {
     fix = gps.read();
     doSomeWork();
   }
+  
   if (PRINT_ACC == 1) {
     //if (Wire.available()) {
-    Acc_Print();
+    accprint(RFDPort);
     //}
   }
+}
+//------------------------------------
 
 
-} // GPSloop
-
-
-void Acc_Print()
+// prints accelerometer data to port specified in function call
+void accprint(Stream &myPort)
 {
 
   gyro_acc.readGyro();
   gyro_acc.readAcc();
   mag.read();
 
-  PressMb  = ps.readPressureMillibars();
-  tempC = ps.readTemperatureC();
+  myPort.print("IMU:");
+  myPort.print(ps.readPressureMillibars());
+  myPort.print(",");
+  myPort.print(ps.readTemperatureC());
+  myPort.print(",");
 
-  IMU[0] = gyro_acc.g.x;
-  IMU[1] = gyro_acc.g.y;
-  IMU[2] = gyro_acc.g.z;
-  IMU[3] = gyro_acc.a.x; //>> 4; // shift right 4 bits to use 12-bit representation (1 g = 256)
-  IMU[4] = gyro_acc.a.y; //>> 4;
-  IMU[5] = gyro_acc.a.z; //>> 4;
-  IMU[6] = mag.m.x;
-  IMU[7] = mag.m.y;
-  IMU[8] = mag.m.z;
+  myPort.print(gyro_acc.g.x);
+  myPort.print(",");
+  myPort.print(gyro_acc.g.y);
+  myPort.print(",");
+  myPort.print(gyro_acc.g.z);
+  myPort.print(",");
+
+  myPort.print(gyro_acc.a.x); //>> 4; // shift right 4 bits to use 12-bit representation (1 g = 256)
+  myPort.print(",");
+  myPort.print(gyro_acc.a.y); //>> 4;
+  myPort.print(",");
+  myPort.print(gyro_acc.a.z); //>> 4;
+  myPort.print(",");
+
+  myPort.print(mag.m.x);
+  myPort.print(",");
+  myPort.print(mag.m.y);
+  myPort.print(",");
+  myPort.print(mag.m.z);
+  myPort.println();
   //
 
-  //RFDPort.print("IMU:");
+  //
   //RFDPort.println(IMU[0]);
 
-  printdata(RFDPort);
+  // printdata(RFDPort);
 
 
 }
 
-void printdata(Stream &myPort ) {
 
-  myPort.write("IMU:");
-
-  //for (int i = 0; i < (sizeof(IMU) / sizeof(IMU[0])); i++) {
-  for (int i = 0; i < 8; i++) {
-    myPort.print(IMU[i]);
-    myPort.print(",");
-  }
-  myPort.print(PressMb);
-  myPort.print(",");
-  myPort.print(tempC);
-  myPort.println();
-
-}
-
-void gpsprint() {
+// prints data stored in GPS fix structure to port specified in function call
+void gpsprint(Stream &myPort) {
   if (fix.valid.location) {
-  RFDPort.print("GPS:");
-  //trace_all( RFDPort, gps, fix);
-  RFDPort.print(fix.dateTime.date);
-  RFDPort.print(fix.dateTime.month);
-  RFDPort.print(fix.dateTime.year);
-  RFDPort.print("-");
-  RFDPort.print(fix.dateTime.hours);
-  RFDPort.print(fix.dateTime.minutes);
-  RFDPort.print(fix.dateTime.seconds);
-  RFDPort.print(fix.dateTime_ms());
-  RFDPort.print(",");
-  RFDPort.print(fix.altitude_cm());
-  RFDPort.print(",");
-  RFDPort.print(fix.latitudeL());
-  RFDPort.print(",");
-  RFDPort.print(fix.longitudeL());
-  RFDPort.print(",");
-  RFDPort.print(fix.speed_mkn());
-  RFDPort.print(",");
-  RFDPort.print(fix.heading_cd());
-  RFDPort.println();
+    myPort.print("GPS:");
+    //trace_all( RFDPort, gps, fix);
+    myPort.print(fix.dateTime.date);
+    myPort.print(fix.dateTime.month);
+    myPort.print(fix.dateTime.year);
+    myPort.print("-");
+    myPort.print(fix.dateTime.hours);
+    myPort.print(fix.dateTime.minutes);
+    myPort.print(fix.dateTime.seconds);
+    myPort.print(fix.dateTime_ms());
+    myPort.print(",");
+    myPort.print(fix.altitude_cm());
+    myPort.print(",");
+    myPort.print(fix.latitudeL());
+    myPort.print(",");
+    myPort.print(fix.longitudeL());
+    myPort.print(",");
+    myPort.print(fix.speed_mkn());
+    myPort.print(",");
+    myPort.print(fix.heading_cd());
+    myPort.println();
   }
 }
